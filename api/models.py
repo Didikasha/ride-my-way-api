4fc9457e38f6d53e67a98d845dfefd3cc33e300f
Didi Kashemwa <didikashemwa@gmail.com>
2018-06-22 19:58:39 +0300

    [Chore #158562480] add Ride modificatio in models file

--- api/models.py
+++ api/models.py
@@ -10,7 +10,24 @@ class User():
 
 class User_Schema(Schema):
     username = fields.Str()
+    fullname = fields.Str()
     email = fields.Str()
     password = fields.Str()
-    
+
+class Ride():
+    def __init__(self, ride_id, driver_name, destination, price, date, time):
+        self.ride_id = ride_id
+        self.driver_name = driver_name
+        self.destination = destination
+        self.price = price
+        self.date = date
+        self.time = time
+
+class Ride_Schema():
+    ride_id=fields.Int()
+    driver_name = fields.Str()
+    destination = fields.Str()
+    price= fields.Int()
+    date = fields.Int()
+    time = fields.Int()
 
