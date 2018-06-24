50b2aff049a09c2dadca8eef915a95bd016dba04
Didi Kashemwa <didikashemwa@gmail.com>
2018-06-25 01:59:08 +0300

    [Feature #158564133] refactor user registration details

--- api/views.py
+++ api/views.py
@@ -11,31 +11,31 @@ api = Api(app)
 users = [User(username="Dee", fullname="Didi Kashemwa",
               email="didikashemwa@gmail.com", password="Yaay")]
 rides = [Ride(ride_id=1234, driver_name="John Doe",
-              destination="Karen", price=250, date=12/12/18, time=1830)]
+              destination="Karen", price=250, date="12/12/18", time=1830)]
 requests = [Request(ride_id=1234, customer_name="Jane Doe",
-              destination="Karen", price=250, date=12/12/18, time=1830)]
+              destination="Karen", price=250, date="12/12/18", time=1830)]
 
 class UserSignupApi(Resource):
     def post(self):
-        user=request.get_json
-        if user.get('username') is None or user.get('fullname') is None or user.get ('email') or user.get('password') is None:
+        user=request.get_json()
+        if not user['username']  or not user['fullname'] or not user['email'] or not user['password']:
             result = jsonify({'message': 'All fields required'}) 
             result.status_code=400
-            return  result
+            return result
 
-        u = User(username=user.get('username'), fullname=user.get('fullname'), email=user.get('email'), password=user.get('password'))
+        u = User(user['username'], user['fullname'],user['email'], user['password'])
 
         users.append(u)
 
-        result = jsonify({'message': 'Successfully registered'})
+        result = jsonify({"message": "Successfully registered"})
         result.status_code = 201
         return result
 
 class UserLoginApi(Resource):
     def post (self):
         access = request.get_json()
-        username = access.get('username')
-        password = access.get('password')
+        username = access['username']
+        password = access['password']
         for user in users:
             if username == user.username:
                 if password ==user.password:
@@ -43,38 +43,36 @@ class UserLoginApi(Resource):
                     result.status_code = 200
                     return result
             else:
-                result = jsonify({'message': 'Wrong password.'})
+                result = jsonify({"message": 'Wrong password or username.'})
                 result.status_code = 401
                 return result
 
 
 
+class RidesApi(Resource):
+    def post(self):
+        new_rides = request.get_json()
+        r = Ride(new_rides['ride_id'], new_rides['driver_name'],new_rides['destination'], new_rides['price'], new_rides['date'], new_rides['time'])
 
+        rides.append(r)
 
-# class RidesApi(Resource):
-#     def post(self):
-#         rides = request.get_json()
-#         r = Ride(ride_id=rides.get('ride_id'), driver_name=rides.get('driver_name'), destination=rides.get(
-#             'destination'), price=rides.get('price'), date=rides.get('date'), time=rides.get('time'))
+        result = jsonify({"message": "ride added"})
+        result.status_code = 201
+        return result
 
-#         rides.append(r)
-
-#         result = jsonify({"message": "ride added"})
-#         result.status_code = 201
-#         return result
-
-    # def get(self):
-    #     ride = Ride_Schema(many = True)
-    #     ride_items = ride.dump(rides)
-
-        # result=jsonify()
+    def get(self):
+        # ride = Ride_Schema(many = True)
+        # ride_items = ride.dump(rides)
+        # result=jsonify(ride_items)
+        itemlist=[]
+        for item in rides:
+            itemlist.append({"Ride Id":item.ride_id, "Driver name": item.driver_name , "destination": item.destination})
+        return itemlist
 
 
 api.add_resource(UserSignupApi, '/api/v1/user/signup')
 api.add_resource(UserLoginApi, '/api/v1/user/login')
-
-
-# api.add_resource(RidesApi,'/api/v1/rides')
+api.add_resource(RidesApi,'/api/v1/rides')
 
 
 if __name__ == '__main__':
