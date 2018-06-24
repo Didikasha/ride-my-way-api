588403658f3ceec7dda9b73112c47f6941fdbe27
Didi Kashemwa <didikashemwa@gmail.com>
2018-06-24 17:48:41 +0300

    [Feature #158564133] implement login test function

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from api.models import User, Ride,Request, User_Schema, Ride_Schema, Request_Sch
 app = Flask(__name__)
 api = Api(app)
 
-users = [User(username="Didi", fullname="Didi Kashemwa",
+users = [User(username="Dee", fullname="Didi Kashemwa",
               email="didikashemwa@gmail.com", password="Yaay")]
 rides = [Ride(ride_id=1234, driver_name="John Doe",
               destination="Karen", price=250, date=12/12/18, time=1830)]
@@ -30,6 +30,23 @@ class UserSignupApi(Resource):
         result = jsonify({'message': 'Successfully registered'})
         result.status_code = 201
         return result
+
+class UserLoginApi(Resource):
+    def post (self):
+        access = request.get_json()
+        username = access.get('username')
+        password = access.get('password')
+        for user in users:
+            if username == user.username:
+                if password ==user.password:
+                    result = jsonify({"message": "You are successfully logged in"})
+                    result.status_code = 200
+                    return result
+            else:
+                result = jsonify({'message': 'Wrong password.'})
+                result.status_code = 401
+                return result
+
 
 
 
@@ -54,6 +71,8 @@ class UserSignupApi(Resource):
 
 
 api.add_resource(UserSignupApi, '/api/v1/user/signup')
+api.add_resource(UserLoginApi, '/api/v1/user/login')
+
 
 # api.add_resource(RidesApi,'/api/v1/rides')
 
