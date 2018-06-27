23febb20f72c5ae7ad7b0b8e5a9b200194a0ff91
Didi Kashemwa <didikashemwa@gmail.com>
2018-06-27 08:20:02 +0300

    [ch]refactor model class, remove marshmallow and schema

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
-from marshmallow import Schema, fields
-import random
+# from marshmallow import Schema, fields
+# import random
 
 class User():
     def __init__(self,username, fullname, email, password):
@@ -8,11 +8,16 @@ class User():
         self.email = email
         self.password = password
 
-class User_Schema(Schema):
-    username = fields.Str()
-    fullname = fields.Str()
-    email = fields.Str()
-    password = fields.Str()
+# class User_Schema(Schema):
+#     username = fields.Str()
+#     fullname = fields.Str()
+#     email = fields.Str()
+#     password = fields.Str()
+
+def __str__(self):
+    '''string repo for user objects''''
+    return '<User:{}'.format(self.username)
+
 
 class Ride():
     def __init__(self, ride_id, driver_name, destination, price, date, time):
@@ -23,22 +28,25 @@ class Ride():
         self.date = date
         self.time = time
 
-class Ride_Schema():
-    ride_id=fields.Int()
-    driver_name = fields.Str()
-    destination = fields.Str()
-    price= fields.Int()
-    date = fields.Int()
-    time = fields.Int()
+# class Ride_Schema():
+#     ride_id=fields.Int()
+#     driver_name = fields.Str()
+#     destination = fields.Str()
+#     price= fields.Int()
+#     date = fields.Int()
+#     time = fields.Int()
+
+def __str__(self):
+    return '<Ride:{}'.format(self.driver)
 
 class Request():
     def __init__(self,ride_id, customer_id):
         self.ride_id = random.randint(1,100)
         self.customer_id=random.randint(1,100)
         
-class Request_Schema():
-    ride_id=fields.Int()
-    customer_id = fields.Int()
+# class Request_Schema():
+#     ride_id=fields.Int()
+#     customer_id = fields.Int()
     
 
 
